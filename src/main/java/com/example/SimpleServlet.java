2af6ed0d02964fbaebc2105edd2d5bc9e887a4c9
Shariful Nibir <nibirsharif@gmail.com>
2018-08-19 01:20:47 +0800

    added do post method

--- src/main/java/com/example/SimpleServlet.java
+++ src/main/java/com/example/SimpleServlet.java
@@ -18,4 +18,14 @@ public class SimpleServlet extends HttpServlet {
             resp.getWriter().write("Please enter a name");
         }
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        if (name != null && name != "") {
+            resp.getWriter().printf("Hello %s", name);
+        } else {
+            resp.sendRedirect("index.jsp");
+        }
+    }
 }
--- target/webapp-1.0-SNAPSHOT/index.jsp
+++ target/webapp-1.0-SNAPSHOT/index.jsp
@@ -1,5 +1,15 @@
 <html>
+    <head>
+        <title>Simple Java Servlet</title>
+    </head>
     <body>
-        <h2>Hello World!</h2>
+        <form action="home" method="post">
+            <p>
+                <input type="text" name="name" />
+            </p>
+            <p>
+                <input type="submit" value="Submit" />
+            </p>
+        </form>
     </body>
 </html>
