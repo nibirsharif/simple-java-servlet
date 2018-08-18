9376ca3f899795d5f09050f6cdc82bee6d963a3f
Shariful Nibir <nibirsharif@gmail.com>
2018-08-19 01:08:56 +0800

    use of get parameter

--- src/main/java/com/example/SimpleServlet.java
+++ src/main/java/com/example/SimpleServlet.java
@@ -7,10 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/home")
+@WebServlet(urlPatterns = {"/home", "/index"})
 public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Hello world!");
+        String name = req.getParameter("name");
+        if (name != null) {
+            resp.getWriter().printf("Hello %s", name);
+        } else {
+            resp.getWriter().write("Please enter a name");
+        }
     }
 }
